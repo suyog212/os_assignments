298d22f1ee8f9c5eb48a13e080a6f58a927fb593
suyog212 <130860821+suyog212@users.noreply.github.com>
2023-10-11 14:46:26 +0530

    Update server.c

--- Assignment_5/server.c
+++ Assignment_5/server.c
@@ -1,43 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
 #include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 
-#define SHM_SIZE 1024  // Size of the shared memory segment
+int main(){
+    int key = ftok("shmfile", 65);
+    int shmid = shmget(key,1024,0666|IPC_CREAT); 
 
-int main() {
-    key_t key = ftok("shmfile", 'R'); // Generate a unique key
-    int shmid;
+    char *str = (char*) shmat(shmid,(void*)0,0); 
+  
+    printf("Write data : ");
+    scanf("%s",str);
+  
+    printf("Data written in memory: %s\n",str); 
+      
+    //detach from shared memory  
+    shmdt(str); 
 
-    // Create a shared memory segment
-    shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
-    if (shmid == -1) {
-        perror("shmget");
-        exit(1);
+    pid_t pid = fork();
+
+    if(pid == 0){
+    char *str = (char*) shmat(shmid,(void*)0,0); 
+    
+    printf("Child Process : ");
+    printf("Data read from memory: %s\n",str); 
+      
+    //detach from shared memory  
+    shmdt(str); 
+    
+    // destroy the shared memory 
+    shmctl(shmid,IPC_RMID,NULL); 
     }
-
-    // Attach the shared memory segment to the server's address space
-    char *shm_ptr = (char *)shmat(shmid, NULL, 0);
-    if (shm_ptr == (char *)(-1)) {
-        perror("shmat");
-        exit(1);
-    }
-
-    // Write a message to the shared memory
-    strcpy(shm_ptr, "Hello from the server!");
-
-    // Wait for the client to signal that it has read the message
-    while (*shm_ptr != '*') {
-        sleep(1);
-    }
-
-    // Detach the shared memory segment
-    shmdt(shm_ptr);
-
-    // Remove the shared memory segment
-    shmctl(shmid, IPC_RMID, NULL);
-
-    return 0;
 }
